a3aa932e2be29902274bb53e3a4ea6d5f6346d47
ramakrishnarg <ramakrishnarg@ramakrishnasmbp>
2017-02-25 10:09:27 -0500

    2nd commit

---
+++ greeting-service/src/main/java/com/example/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+	
+	@Value("${greeting}")
+	private String greeting;
+
+	@RequestMapping(method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return greeting;
+    }
+}
