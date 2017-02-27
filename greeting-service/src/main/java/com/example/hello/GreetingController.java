21ee5cf6bde54624fb09b436727dd3a50ccf048e
ramakrishnarg <ramakrishnarg@ramakrishnasmbp>
2017-02-27 10:44:18 -0500

    changes to make work refreshScope in spring cloud

--- greeting-service/src/main/java/com/example/hello/GreetingController.java
+++ greeting-service/src/main/java/com/example/hello/GreetingController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+
 @RestController
+@RefreshScope
 @RequestMapping("/greeting")
 public class GreetingController {
 	
