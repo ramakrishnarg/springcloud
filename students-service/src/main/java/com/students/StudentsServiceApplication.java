0de30901e703fb4c291af975d0da82c43a9c00ae
ramakrishnarg <ramakrishnarg@ramakrishnasmbp>
2017-02-27 13:43:54 -0500

    dd

--- students-service/src/main/java/com/students/StudentsServiceApplication.java
+++ students-service/src/main/java/com/students/StudentsServiceApplication.java
@@ -52,7 +52,6 @@ class SampleCLR implements CommandLineRunner{
 		Stream.of("Ram","Anu","Siri","Krish","Reddi").forEach(name->studentRepository.save(new Student(name)));
 		studentRepository.findAll().forEach(System.out::println);
 	}
-	//this is dummy committ
 	
 }
 
