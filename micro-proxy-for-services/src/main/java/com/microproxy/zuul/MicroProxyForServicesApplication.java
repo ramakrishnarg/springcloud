047115ef145eb9a0e9c1d53ee27a994e17256315
ramakrishnarg <ramakrishnarg@ramakrishnasmbp>
2017-02-26 22:39:30 -0500

    addind eureka and microproxy services

---
+++ micro-proxy-for-services/src/main/java/com/microproxy/zuul/MicroProxyForServicesApplication.java
@@ -0,0 +1,17 @@
+package com.microproxy.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroProxyForServicesApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MicroProxyForServicesApplication.class, args);
+	}
+}
