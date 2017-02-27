7b0cbd40cc6b998ed032b4d331f27282ced84505
ramakrishnarg <ramakrishnarg@ramakrishnasmbp>
2017-02-26 22:38:30 -0500

    changes

--- greeting-service/src/main/java/com/example/hello/GreetingController.java
+++ greeting-service/src/main/java/com/example/hello/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.hello;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
